e195daf77bbe65209e61b4febd2c374f722ebb7b
Joshua B <barrs@chapman.edu>
2021-03-21 20:16:56 -0700

    Added components of assignment3

---
+++ Assignment3/assignment3/StudentProgram.py
@@ -0,0 +1,274 @@
+import sqlite3
+import re # for regular expression checking (mobile phone number and gpa)
+import pandas as pd
+
+
+conn = sqlite3.connect('./student.db')
+mycursor = conn.cursor()
+
+def searchDB():
+    mycursor = conn.execute("SELECT * FROM Student")
+    conn.commit()
+
+    for i in mycursor:
+        print(i)
+
+def createNewStudent():
+    stuValues = [] # an empty list which will contain the numeric properties/values of the new student being added to the student DB. This will be used in the below function to validate the numeric parameters/values
+    print("Please input the following parameters for the new student: StudentID Number, FirstName, LastName, GPA, Major, FacultyAdvisor, Address (only street address), City, State, ZIP Code, MobilePhoneNumber")
+
+    stuID = input("Please enter the student ID Number, then press \'Enter\'")
+    validateInput(stuID)
+    stuValues.append(stuID)
+
+    fName = input("Please enter the firstname of the student, then press \'Enter\'")
+    stuValues.append(fName)
+
+    lName = input("Please enter the lastname of the student, then press \'Enter\'")
+    stuValues.append(lName)
+
+    gpa = input("Please enter the GPA of the student, rounded to the nearest tenth")
+    validateInput(gpa)
+    stuValues.append(gpa)
+
+    major = input("Please enter the student's major")
+    stuValues.append(major)
+
+    facultyAdvisor = input("Please enter the first and last name of the student's faculty advisor. If the student has no faculty advisor, enter \'None\'")
+    stuValues.append(facultyAdvisor)
+
+    address = input("Please enter the street address (ONLY) of the student")
+    stuValues.append(address)
+
+    city = input("Please enter the student's city that pertains to his/her street address")
+    stuValues.append(city)
+
+    state = input("Please enter the student's state that pertains to his/her street address")
+    stuValues.append(state)
+
+    zip = input("Please enter the student's ZIP code that pertains to his/her street address")
+    validateInput(zip)
+    stuValues.append(zip)
+
+    phonenum = input("Please enter the student's mobile phone number, in the following format: (xxx) xxx-xxxx. You MUST input the phone number in this format or the program will throw an error")
+    validateInput(phonenum)
+    stuValues.append(phonenum)
+
+    #below we are checking to make sure the user did not leave any fields blank
+    for i in stuValues:
+        stringNotEmpty(i)
+
+    #if the user is being added, he is obviously not deleted, so set isDeleted to False (or 0)
+    stuValues.append(False)
+
+    conn.execute("INSERT INTO Student VALUES(?,?,?,?,?,?,?,?,?,?,?,?)", stuValues)
+    conn.commit()
+
+    print("Student with id {} added".format(stuID))
+
+def userExists(idNum):
+    result = conn.execute("SELECT COUNT(*) FROM Student S WHERE S.StudentID = ?", idNum)
+    conn.commit()
+
+    # making sure the requested student ID# is in the database
+    for i in result:
+        for j in i:
+            if j == 0:  # if found == 0
+                print("Error! No student with requested ID # found in the Database. Please try again")
+                return 0
+    else:
+        return 1
+
+
+def updateStudent():
+    IDNumList = [] # list to be used in conjunction with the SQL statement below
+    IDNum= input("Please input the ID# of the student you would like to update: ")
+    validateInput(IDNum) # validate the ID #
+    int(IDNum) # type casting
+    IDNumList.append(IDNum)
+    userExists(IDNumList)
+    # result = conn.execute("SELECT COUNT(*) FROM Student S WHERE S.StudentID = ?", IDNumList)
+    # conn.commit()
+    #
+    # #making sure the requested student ID# is in the database
+    # for i in result:
+    #     for j in i:
+    #         if j == 0: # if found == 0
+    #             print("Error! No student with requested ID # found in the Database. Please try again")
+    #             return 0
+
+    majorUpdate = input("Would you like to update student's major? (Y/N)")
+    advisorUpdate = input("Would you like to update student's faculty advisor? (Y/N)")
+    mobilephoneUpdate = input("Would you like to update student's mobile phone number? (Y/N)")
+
+    if majorUpdate == "Y" or majorUpdate == "y":
+        queryParams = [] # list to be used in conjunction with the SQL statement below
+        newMajor = input("What is the student's new major?")
+        queryParams.append(newMajor)
+        queryParams.append(IDNum)
+        conn.execute("UPDATE Student SET Major = ? WHERE Student.StudentID = ?", queryParams)
+        conn.commit()  # commit
+        print("Major field has been updated")
+
+    if advisorUpdate == "Y" or advisorUpdate == "y":
+        queryParams = []  # list to be used in conjunction with the SQL statement below
+        newFacultyAdvisor = input("Who is the student's new faculty advisor? Enter the first and last name of the faculty member")
+        queryParams.append(newFacultyAdvisor)
+        queryParams.append(IDNum)
+        conn.execute("UPDATE Student SET FacultyAdvisor = ? WHERE Student.StudentID = ?", queryParams)
+        conn.commit()  # commit
+        print("Faculty advisor field has been updated")
+
+
+    if mobilephoneUpdate == "Y" or mobilephoneUpdate == "y":
+        queryParams = []  # list to be used in conjunction with the SQL statement below
+        newMobilePhone = input("What is the student's new mobile phone number? You must enter the phone number in the following format: (xxx) xxx-xxxx or the program will reject the update")
+        queryParams.append(newMobilePhone)
+        queryParams.append(IDNum)
+        validateInput(newMobilePhone) #validate the new mobile phone number
+        conn.execute("UPDATE Student SET MobilePhoneNumber = ? WHERE Student.StudentID = ?", queryParams)
+        conn.commit()  # commit
+        print("Mobile Phone Number field has been updated")
+
+def deleteStudentByID():
+    IDNumList = []  # list to be used in conjunction with the SQL statement below
+    IDNum = input("Please input the ID# of the student you would like to delete: ")
+    validateInput(IDNum)  # validate the ID #
+    int(IDNum)  # type casting
+    IDNumList.append(IDNum) # set isDeleted to True (or 1)
+    if (not userExists(IDNumList)):
+        print("User not found! Enter a new ID")
+        return 1
+    # result = conn.execute("SELECT COUNT(*) FROM Student S WHERE S.StudentID = ?", IDNumList)
+    # conn.commit()
+    #
+    # # making sure the requested student ID# is in the database
+    # for i in result:
+    #     for j in i:
+    #         if j == 0:  # if found == 0
+    #             print("Error! No student with requested ID # found in the Database. Please try again")
+    #             return 0
+
+    # if the student with the requested ID # is in the database
+    queryParams = [1, IDNum]
+    conn.execute("UPDATE Student SET isDeleted =  ? WHERE Student.StudentID = ?", queryParams) # delete the student with the requested student id # from the list of students in the database
+    conn.commit()
+    print("Student with ID number: {} has been deleted".format(queryParams[1]))
+
+
+
+def stringNotEmpty(value):
+    if value == "":
+        print("Value cannot be empty. All input fields are required")
+        exit(1)
+
+def validateInput(num):
+    pattern = "^\d{1}.\d{1}"
+    pattern2 = "^\((\d{3})\) (\d{3})-(\d{4})$" # for mobile phone number
+    isValid = re.match(pattern, num)
+    isValid2 = re.match(pattern2, num)
+
+    if num.isnumeric():
+        return True
+    elif isValid:
+        return True
+    elif isValid2: # if the mobile phone number is valid
+        return True
+    else:
+        print("Error on one of the numeric inputs. Please check the inputs")
+        exit(1)
+
+def searchStudentsByAttribute():
+    searchParam = input("How would you like to search students? Type Major, GPA, City, State or Advisor. NOTE: Nothing returned indicates that there are no students matching the requested criteria/attributes")
+
+    if searchParam == "Major":
+        majorToSearch = input("What is the major by which you would like to search the student DB?")
+        major = [majorToSearch]
+        cursor = conn.execute("SELECT * FROM Student S WHERE S.Major = ?", major)
+
+        for i in cursor:
+            print(i)
+
+    elif searchParam == "GPA":
+        GPAToSearch = input("What is the GPA by which you would like to search the student DB?")
+        theGPA = [GPAToSearch]
+        cursor = conn.execute("SELECT * FROM Student S WHERE S.GPA = ?", theGPA)
+
+        for i in cursor:
+            print(i)
+
+    elif searchParam == "City":
+        cityToSearch = input("What is the city by which you would like to search the student DB?")
+        city = [cityToSearch]
+        cursor = conn.execute("SELECT * FROM Student S WHERE S.City = ?", city)
+
+        for i in cursor:
+            print(i)
+
+    elif searchParam == "State":
+        stateToSearch = input("What is the state by which you would like to search the student DB?")
+        state = [stateToSearch]
+        cursor = conn.execute("SELECT * FROM Student S WHERE S.State = ?", state)
+
+        for i in cursor:
+            print(i)
+
+    elif searchParam == "Advisor":
+        advisorToSearch = input("What is the name of the faculty advisor (first and last name) by which you would like to search the student DB?")
+        advisor = [advisorToSearch]
+        cursor = conn.execute("SELECT * FROM Student S WHERE S.Major = ?", advisor)
+
+        for i in cursor:
+            print(i)
+
+
+
+if __name__ == "__main__":
+    userOption = int(input("Welcome to the student database program. From here, you will be able to insert, update, delete, or search your students that are currently in the student database. \n"
+          "Please indicate the option you would like to perform by typing the number associated with that option: \n 1: Display all students currently in the database as well as their attributes \n 2: Add in a new student into the database"
+          "\n 3: Update a current student (requires the student ID number) \n 4: Delete student by student ID number \n 5: Search/Display students by either Major, GPA, City, State, or Advisor. Type \'0\' to quit"))
+
+    while userOption != 0:
+
+        if userOption == 1:
+            searchDB()
+            print() # formatting
+            print("Please note that a 0 in the last attribute field of the student indicates that the student is not deleted in the database, while a value of 1 indicates the student is deleted from the database")
+            # user clarification in above print statement
+            print() # formatting
+            userOption = int(input("Please type another option, or \'0\' to quit.\n 1: Display all students currently in the database as well as their attributes \n 2: Add in a new student into the database"
+            "\n 3: Update a current student (requires the student ID number) \n 4: Delete student by student ID number \n 5: Search/Display students by either Major, GPA, City, State, or Advisor."))
+
+
+        elif userOption == 2:
+            createNewStudent()
+            print() # formatting
+            userOption = int(input("Please type another option, or \'0\' to quit.\n 1: Display all students currently in the database as well as their attributes \n 2: Add in a new student into the database"
+            "\n 3: Update a current student (requires the student ID number) \n 4: Delete student by student ID number \n 5: Search/Display students by either Major, GPA, City, State, or Advisor."))
+
+        elif userOption == 3:
+            updateStudent()
+            print() # formatting
+            userOption = int(input("Please type another option, or \'0\' to quit.\n 1: Display all students currently in the database as well as their attributes \n 2: Add in a new student into the database"
+            "\n 3: Update a current student (requires the student ID number) \n 4: Delete student by student ID number \n 5: Search/Display students by either Major, GPA, City, State, or Advisor."))
+
+        elif userOption == 4:
+            deleteStudentByID()
+            print() # formatting
+            userOption = int(input("Please type another option, or \'0\' to quit.\n 1: Display all students currently in the database as well as their attributes \n 2: Add in a new student into the database"
+            "\n 3: Update a current student (requires the student ID number) \n 4: Delete student by student ID number \n 5: Search/Display students by either Major, GPA, City, State, or Advisor."))
+
+        elif userOption == 5:
+            searchStudentsByAttribute()
+            print() # formatting
+            userOption = int(input("Please type another option, or \'0\' to quit.\n 1: Display all students currently in the database as well as their attributes \n 2: Add in a new student into the database"
+            "\n 3: Update a current student (requires the student ID number) \n 4: Delete student by student ID number \n 5: Search/Display students by either Major, GPA, City, State, or Advisor."))
+
+        elif userOption == 0:
+            print("Exitting program. Good-bye!")
+            exit(0)
+
+        else:
+            print("Input not recognized. Please type a value between 1 and 5 (inclusive)")
+            continue # go back to the top of the while loop
+
---
+++ Assignment3/assignment3/data-ingestion.py
@@ -0,0 +1,35 @@
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect('./student.db')
+mycursor = conn.cursor()
+
+# student_data = pd.read_csv('./students.csv', header=None, skiprows=[0])
+with open('./students.csv') as inputfile:
+    column_names = inputfile.readline()
+    student_data = inputfile.readlines() # read in all the lines in the students.csv file
+
+# print(student_data)
+
+for row in student_data:
+    listOfStudents = []
+    row_list = row.split(',') # split the values in the row into an array of values
+    # int(row_list[5])
+    # float(row_list[8])
+    # for i in row_list:
+    #     print(i)
+    #     listOfStudents.append(i)
+    conn.execute("INSERT INTO Student(FirstName, LastName, Address, City, State, ZipCode, MobilePhoneNumber, MAJOR, GPA) VALUES(?,?,?,?,?,?,?,?,?)", row_list)
+
+print(len(row_list))
+
+conn.commit()
+
+
+
+
+
+
+# for row in student_data.iterrows():
+#     conn.executemany("INSERT INTO Student(FirstName, LastName, Address, City, State, ZipCode, MobilePhoneNumber, MAJOR, GPA) VALUES(?,?,?,?,?,?,?,?,?)", row)
+#     print("entered info")
